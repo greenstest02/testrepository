dac771d8c969e69704c051b46c8bdf53378d9e1b
greenstest02 <greenstest02@gmail.com>
2022-02-06 21:18:37 +0530

    Task Done

---
+++ src/com/greens/testtwo/GithubPush.java
@@ -0,0 +1,14 @@
+package com.greens.testtwo;
+
+public class GithubPush {
+
+	private void add() {
+		System.out.println("Hi Ashik");
+	}
+	
+	public static void main(String[] args) {
+		GithubPush a = new GithubPush();
+		a.add();
+		
+	}
+}
